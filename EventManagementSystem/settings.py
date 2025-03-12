de51457ffcf15e5b83cebca1ab135d82a2b3c1a5
Harsha Vardhan Reddy Bindela <harshavardhanreddy.bindela@slu.edu>
2025-03-12 18:24:18 -0400

    email hidden

--- EventManagementSystem/settings.py
+++ EventManagementSystem/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9l^8zr593y*=dj3@eo+vwa%u(v)9o9@kjxfq=mw==$pmx))_%q'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -30,13 +32,17 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+# Email Settings
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "harshareddy4400@gmail.com"  # Replace with your email
-EMAIL_HOST_PASSWORD = "mbza hczr lhze unas"  # Use an app password for security
-DEFAULT_FROM_EMAIL = "harshareddy4400@gmail.com"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")  # Read from .env
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")  # Read from .env
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")  # Read from .env
+
 
 
 
